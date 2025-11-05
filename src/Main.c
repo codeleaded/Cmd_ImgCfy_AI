edc1d632dca0d256636308a9b4b77511dea2063d
codeleaded <codeleaded@gmail.com>
2025-11-05 12:54:35 +0100

    update

--- src/Main.c
+++ src/Main.c
@@ -1,17 +1,26 @@
-#include "/home/codeleaded/System/Static/Library/NeuralNetwork.h"
+#include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
 #include "/home/codeleaded/System/Static/Library/GSprite.h"
-#include "/home/codeleaded/System/Static/Library/String_G.h"
+#include "/home/codeleaded/System/Static/Library/NeuralNetwork.h"
 
 #define SPRITE_PATH         "/home/codeleaded/Data/NN/Digits/"
+#define DATA_PATH           "/home/codeleaded/Data/NN/DigitsGray/"
 #define SPRITE_TEST         "testing"
 #define SPRITE_TRAINING     "training"
-#define SPRITE_COUNT        10
+#define SPRITE_COUNT        4
+#define SPRITE_MAX          300
 
-#define NN_LEARNRATE        0.5f
+#define NN_PATH             "./data/Model.nnalx"
+#define NN_COUNT            10
+#define NN_LEARNRATE        0.1f
 
 int main(){
     NeuralNetwork nn = NeuralNetwork_Make(
-        (NeuralLayerCount[]){ 784,128,10,NeuralLayerCount_End }
+        (NeuralLayerBuilder[]){ 
+            NeuralLayerBuilder_Make(784,"relu"),
+            NeuralLayerBuilder_Make(128,"relu"),
+            NeuralLayerBuilder_Make(10,"softmax"),
+            NeuralLayerBuilder_End()
+        }
     );
 
     for(int i = 0;i<10;i++){
@@ -24,7 +33,13 @@ int main(){
             memset(outs,0,sizeof(outs));
             outs[i] = 1.0f;
 
-            NeuralNetwork_Training(&nn,sp.img,outs,NN_LEARNRATE);
+            NeuralDataMap ndm = NeuralDataMap_Make_GSprite(DATA_PATH SPRITE_TRAINING);
+            NeuralNetwork_Learn(&nn,&ndm,NN_LEARNRATE);
+            NeuralDataMap_Free(&ndm);
+
+            ndm = NeuralDataMap_Make_GSprite(DATA_PATH SPRITE_TEST);
+            NeuralType loss = NeuralNetwork_Test_C(&nn,&ndm);
+            NeuralDataMap_Free(&ndm);
 
             GSprite_Free(&sp);
         }
